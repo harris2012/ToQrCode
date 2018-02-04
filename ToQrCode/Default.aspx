9ee7837d889042905429e54b71eb1c4ab23eb7b0
HarrisZhang <harriszhang@live.cn>
2018-02-04 22:20:01 +0800

    代码迁移

---
+++ ToQrCode/Default.aspx
@@ -0,0 +1,23 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="ToQrCode.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="ContentTextBox" runat="server" Text="http://192.168.1.100:1234/apk/app-debug.apk"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Button ID="BtStart" runat="server" Text="Button" OnClick="BtStart_Click" />
+        </div>
+        <div>
+            <asp:Image ID="QrImage" runat="server" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ ToQrCode/Default.aspx.cs
@@ -0,0 +1,51 @@
+﻿using QRCoder;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ToQrCode
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void BtStart_Click(object sender, EventArgs e)
+        {
+            InitFolder();
+
+            string path = string.Format("/img/{0:yyyyMMddHHmmss}.jpg", DateTime.Now);
+
+            using (var gen = new QRCodeGenerator())
+            {
+                QRCodeData qrCodeData = gen.CreateQrCode(ContentTextBox.Text, QRCodeGenerator.ECCLevel.Q);
+
+                QRCode qrCode = new QRCode(qrCodeData);
+
+                Bitmap img = qrCode.GetGraphic(10);
+
+                img.Save(Server.MapPath(path), ImageFormat.Jpeg);
+            }
+
+            QrImage.ImageUrl = path;
+        }
+
+        private void InitFolder()
+        {
+            var path = Server.MapPath("/img");
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+        }
+    }
+}
